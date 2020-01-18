cbff5123850ac09ff1bbbf55c931f09b5c8c8a06
Rafael Barbosa <rafael.1940.b@gmail.com>
2020-01-18 01:02:07 -0300

    Adicionando o layout para a aplicação

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
+          
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/40358789?s=460&v=4" alt="Rafael Barbosa"/>
+              <div className="user-info">
+                <strong>Rafael Barbosa</strong>
+                <span>ReactJS, React Native, Python</span>
+                <p>Teste takdjaskdl ksadlkamdl kasdkasmdkams</p>
+                <a href="https://github.com/raf21a">Acessar perfil no Github</a>
+              </div>
+            </header>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/40358789?s=460&v=4" alt="Rafael Barbosa"/>
+              <div className="user-info">
+                <strong>Rafael Barbosa</strong>
+                <span>ReactJS, React Native, Python</span>
+                <p>Teste takdjaskdl ksadlkamdl kasdkasmdkams</p>
+                <a href="https://github.com/raf21a">Acessar perfil no Github</a>
+              </div>
+            </header>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/40358789?s=460&v=4" alt="Rafael Barbosa"/>
+              <div className="user-info">
+                <strong>Rafael Barbosa</strong>
+                <span>ReactJS, React Native, Python</span>
+                <p>Teste takdjaskdl ksadlkamdl kasdkasmdkams</p>
+                <a href="https://github.com/raf21a">Acessar perfil no Github</a>
+              </div>
+            </header>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/40358789?s=460&v=4" alt="Rafael Barbosa"/>
+              <div className="user-info">
+                <strong>Rafael Barbosa</strong>
+                <span>ReactJS, React Native, Python</span>
+                <p>Teste takdjaskdl ksadlkamdl kasdkasmdkams</p>
+                <a href="https://github.com/raf21a">Acessar perfil no Github</a>
+              </div>
+            </header>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
