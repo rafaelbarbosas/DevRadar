d75dc7d380638596ec42b7536fafc58402e65ff7
Rafael Barbosa <rafael.1940.b@gmail.com>
2020-01-19 21:43:23 -0300

    Adiciona feature real time, ao adicionar um dev

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
     async index(request, response) {
@@ -33,6 +34,17 @@ module.exports = {
                 techs: techsArray,
                 location
             });
+
+            //Filtrar e mandar para o socket
+            //Mandar pro socket
+
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude},
+                techsArray
+            );
+
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
+
         }
     
         return response.json({dev});
