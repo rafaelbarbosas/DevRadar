185d77f4f757f47f120e0c0caf875896c4241cb5
Rafael Barbosa <rafael.1940.b@gmail.com>
2020-01-19 21:42:08 -0300

    Finaliza aplicação web (desculpa o commit grande)

--- web/src/App.js
+++ web/src/App.js
@@ -1,86 +1,43 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  async function loadDevs()  {
+    const response = await api.get('/devs');
+
+    setDevs(response.data)
+  }
+
+  useEffect( () => loadDevs(), []);
+
+  async function handleAddDev(data) {
+
+    await api.post('devs', data);
+
+    loadDevs();
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-          
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required/>
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required/>
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/40358789?s=460&v=4" alt="Rafael Barbosa"/>
-              <div className="user-info">
-                <strong>Rafael Barbosa</strong>
-                <span>ReactJS, React Native, Python</span>
-                <p>Teste takdjaskdl ksadlkamdl kasdkasmdkams</p>
-                <a href="https://github.com/raf21a">Acessar perfil no Github</a>
-              </div>
-            </header>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/40358789?s=460&v=4" alt="Rafael Barbosa"/>
-              <div className="user-info">
-                <strong>Rafael Barbosa</strong>
-                <span>ReactJS, React Native, Python</span>
-                <p>Teste takdjaskdl ksadlkamdl kasdkasmdkams</p>
-                <a href="https://github.com/raf21a">Acessar perfil no Github</a>
-              </div>
-            </header>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/40358789?s=460&v=4" alt="Rafael Barbosa"/>
-              <div className="user-info">
-                <strong>Rafael Barbosa</strong>
-                <span>ReactJS, React Native, Python</span>
-                <p>Teste takdjaskdl ksadlkamdl kasdkasmdkams</p>
-                <a href="https://github.com/raf21a">Acessar perfil no Github</a>
-              </div>
-            </header>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/40358789?s=460&v=4" alt="Rafael Barbosa"/>
-              <div className="user-info">
-                <strong>Rafael Barbosa</strong>
-                <span>ReactJS, React Native, Python</span>
-                <p>Teste takdjaskdl ksadlkamdl kasdkasmdkams</p>
-                <a href="https://github.com/raf21a">Acessar perfil no Github</a>
-              </div>
-            </header>
-          </li>
+          {devs.map( dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
